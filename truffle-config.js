d42d067617e80d38a0a6bd7b6966a308f43fbbb1
Fundaciongrupoal <56349806+Fundaciongrupoal@users.noreply.github.com>
2019-10-24 14:14:32 +0200

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "195.77.239.98",
+      port: 443,
+      network_id: "50730eaea866",
+      gas: 4700000,
+      gasPrice: 0
+    }
+  }
+};
